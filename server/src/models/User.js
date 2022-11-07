41f2681e744b3c18f3f5868acc30c73021175f6a
ed-uemura <117615570+ed-uemura@users.noreply.github.com>
2022-11-06 22:03:18 -0800

    Update User.js
    Now with the embedded documents

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,145 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
+const PaymentMethodSchema = new mongoose.Schema({
+	type: {
+        type: String,
+        require: true,
+    },
+	pan: {
+        type: String,
+        require: true,
+    },
+	name: {
+        type: String,
+        require: true,
+		max: 30,
+    },
+	expiration: {
+        type: Date,
+        require: true,
+    },
+	isdefault: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+	address_id: {
+        type: String,
+        require: false,
+		max: 30,
+    }, 
+},
+{ timestamps: true }
+);
+
+const AddressSchema = new mongoose.Schema({
+	name: {
+        type: String,
+        require: true,
+    },
+	address: {
+        type: String,
+        require: true,
+    },
+	address2: {
+        type: String,
+        require: false,
+    },
+	city: {
+        type: String,
+        require: true,
+    },
+	province: {
+        type: String,
+        require: true,
+    },
+	zip: {
+        type: String,
+        require: true,
+    },
+	isdefault: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+},
+{ timestamps: true }
+);
+
+const CarSchema = new mongoose.Schema({
+	make: {
+        type: String,
+        require: true,
+    },
+	model: {
+        type: String,
+        require: true,
+    },
+	type: {
+        type: String,
+        require: true,
+    },
+	color: {
+        type: String,
+        require: true,
+    },
+	year: {
+        type: Integer,
+        require: true,
+		min: 1900,
+        max: 2100,
+    },
+	picture: {
+        type: String,
+        require: false,
+    },
+	plate: {
+        type: String,
+        require: true,
+    },
+	feat_ac: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+	feat_ls: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+	feat_lm: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+	feat_ll: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+	feat_wf: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+	feat_bk: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+	feat_sk: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+	feat_pa: {
+        type: Boolean,
+        require: false,
+        default: false,
+    },
+},
+{ timestamps: true }
+);
 
 const UserSchema = new mongoose.Schema({
     firstname: {
@@ -91,6 +232,9 @@ const UserSchema = new mongoose.Schema({
         require: false,
         default: "ACTIVE",
     },
+	cars: [CarSchema],
+	payment_methods: [PaymentMethodSchema],
+	addresses: [AddressSchema]
 },
 { timestamps: true }
 );
